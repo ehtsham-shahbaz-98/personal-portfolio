0fdc386ba859a1a6cf406bafc7660a44bd669eb6
ratroot92 <ratroot92@gmail.com>
2023-04-06 13:23:06 +0500

    update: 1st commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br /> My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hi, my Name is Ehtsham Shahbaz. I am a Full Stack Web Developer. My goal
+        is to convert your ideas into reality in form of highly optimized Web
+        Apps.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
